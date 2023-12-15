648cd4880c5155f53b3a7c60f07d0eba7b704092
Paul <paulwharin@gmail.com>
2023-12-15 15:59:18 +0000

    refactor: wrap form test in a 'describe' block

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,13 +1,16 @@
 import { render } from "@testing-library/react";
 import W12MForm from "./W12MForm";
 
-test("renders form element", () => {
-  // we can hold onto the object returned from render()
-  // this object has a container property that we can destructure and inspect
-  const { container } = render(<W12MForm />);
+describe("W12MForm", () => {
+  test("renders form element", () => {
+    // we can hold onto the object returned from render()
+    // this object has a container property that we can destructure and inspect
+    const { container } = render(<W12MForm />);
 
-  // the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
-  // for example, the firstChild of our container should be our form element
-  /* eslint-disable testing-library/no-node-access */
-  expect(container.firstChild).toHaveClass("w12MForm");
+    // the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
+    // for example, the firstChild of our container should be our form element
+
+    /* eslint-disable testing-library/no-node-access */
+    expect(container.firstChild).toHaveClass("w12MForm");
+  });
 });
